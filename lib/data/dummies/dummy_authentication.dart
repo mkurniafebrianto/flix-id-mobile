25c07af29611f73f31594385d63f69fd3c918e12
mkurniafebrianto <mkurniafebrianto@gmail.com>
2026-01-16 18:24:24 +0700

    add dummy repo

---
+++ lib/data/dummies/dummy_authentication.dart
@@ -0,0 +1,35 @@
+import '../../domain/entities/result.dart';
+import '../repositories/authentication.dart';
+
+class DummyAuthentication implements Authentication {
+  @override
+  String? getLoggedInUserId() {
+    // TODO: implement getLoggedInUserId
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<String>> login({
+    required String email,
+    required String password,
+  }) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return const Result.success('ID-1234');
+    // return const Result.failed('Login Gagal');
+  }
+
+  @override
+  Future<Result<void>> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<String>> register({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+}
